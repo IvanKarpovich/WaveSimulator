e5a297d4e43b7acc6360d9b0ca2505df3d93fa12
IvanKarpovich <Gleb123@sibmail.com>
2020-05-26 11:52:15 +0700

    Create import.py

---
+++ import.py
@@ -0,0 +1,8 @@
+from matplotlib.colors import LinearSegmentedColormap
+from matplotlib import cm
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+import glob
+from PIL import Image
