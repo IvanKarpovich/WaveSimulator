7853bdebac220da05539d1a6193b2993eb307c2e
IvanKarpovich <Gleb123@sibmail.com>
2020-05-27 22:23:09 +0700

    Delete import.py

--- import.py
+++
@@ -1,8 +0,0 @@
-from matplotlib.colors import LinearSegmentedColormap
-from matplotlib import cm
-import matplotlib.pyplot as plt
-import numpy as np
-import math
-
-import glob
-from PIL import Image
